de1116127dc6a9578b316276398d01ebb8696b3a
Zoltan.Szabo <zoltan.szabo.it@gmail.com>
2015-12-10 15:38:54 +0100

    Log4jConfigurator can load log4j config file from class loader

--- src/main/java/org/bidziil/configuration/Log4jConfigurator.java
+++ src/main/java/org/bidziil/configuration/Log4jConfigurator.java
@@ -1,13 +1,14 @@
 package org.bidziil.configuration;
 
-import com.google.common.base.Strings;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.logging.Level;
+
 import org.apache.log4j.Appender;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.xml.DOMConfigurator;
-
-import java.io.File;
-import java.util.Enumeration;
-import java.util.logging.Level;
 
 public class Log4jConfigurator {
 
@@ -30,7 +31,8 @@ public class Log4jConfigurator {
 		if ( isConfigured() ) return;
 
 		try {
-			String file = getFile();
+			String file = tryToGetLog4jConfiguration();
+
 			if ( file.toLowerCase().endsWith(".xml") ) {
 				DOMConfigurator.configureAndWatch(file, getDelay());
 			} else {
@@ -44,12 +46,23 @@ public class Log4jConfigurator {
 		}
 	}
 
+	private String tryToGetLog4jConfiguration() {
+		String file = tryToGetFileFromClassLoader();
+		if ( file == null ) {
+			file = tryToGetFileFromJvm();
+		}
+		if ( file == null ) {
+			throw new NullPointerException("There is no log4j config file!");
+		}
+		return file;
+	}
+
 	public long getDelay() {
 		String refreshIntervalParam = System.getProperty(LOG4J_REFRESH_INTERVAL);
 		long delay = DEFAULT_INTERVAL;
 
 		// Check for refreshIntervalParam parameter
-		if ( !Strings.isNullOrEmpty(refreshIntervalParam) ) {
+		if ( refreshIntervalParam != null && !refreshIntervalParam.isEmpty() ) {
 			try {
 				delay = Long.parseLong(refreshIntervalParam);
 			} catch ( NumberFormatException nfe ) {
@@ -57,15 +70,51 @@ public class Log4jConfigurator {
 				delay = DEFAULT_INTERVAL;
 			}
 		}
-		LOGGER.log(Level.INFO, "delay=" + refreshIntervalParam + " -> " + delay);
+		System.out.println("delay=" + refreshIntervalParam + " -> " + delay);
 		return delay;
 	}
+	
+	public String tryToGetFileFromClassLoader() {
+		try {
+			return getFileFromClassLoader("/WEB-INF/classes/log4j.xml");
+		} catch (Exception e) {
+			System.err.println(String.format("%s occurred while load log4j from classloader due to %s", e.getClass().getSimpleName(), ( e.getMessage() != null ) ? e.getMessage().toLowerCase() : "-"));
+		}
+		return null;
+	}
+	
+	public String getFileFromClassLoader(String resourceName) throws URISyntaxException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		URL resource = classLoader.getResource(resourceName);
+		if ( resource == null ) {
+			throw new URISyntaxException("/WEB-INF/classes/log4j.xml", "log4j.xml is missing");
+		}
 
-	public String getFile() {
+		File file = new File(resource.toURI());
+		
+		if ( !( file.exists() ) ) {
+			String message = String.format("Invalid '%s' parameter value '%s'.", getPropertyName(), file);
+			throw new IllegalArgumentException(message);
+		}
+		
+		System.out.println("log4j file= -> " + file.getAbsolutePath());
+		return file.getAbsolutePath();
+	}
+	
+	public String tryToGetFileFromJvm() {
+		try {
+			return getFileFromJvm();
+		} catch (Exception e) {
+			System.err.println(String.format("%s occurred while load log4j from jvm due to %s", e.getClass().getSimpleName(), ( e.getMessage() != null ) ? e.getMessage().toLowerCase() : "-"));
+		}
+		return null;
+	}
+
+	public String getFileFromJvm() {
 		String configurationParam = System.getProperty(getPropertyName());
 		String file = configurationParam;
 
-		if ( Strings.isNullOrEmpty(file) ) {
+		if ( file == null || file.isEmpty() ) {
 			String message = String.format("Invalid '%s' parameter value '%s'.", getPropertyName(), file);
 			throw new IllegalArgumentException(message);
 		}
@@ -79,7 +128,7 @@ public class Log4jConfigurator {
 			String message = String.format("Invalid '%s' parameter value '%s'.", getPropertyName(), file);
 			throw new IllegalArgumentException(message);
 		}
-		LOGGER.log(Level.INFO, "file=" + configurationParam + " -> " + file);
+		System.out.println("log4j file=" + configurationParam + " -> " + file);
 		return file;
 	}
 
@@ -87,15 +136,24 @@ public class Log4jConfigurator {
 		return propertyName;
 	}
 
+	@SuppressWarnings("unchecked")
 	public boolean isConfigured() {
 		org.apache.log4j.Logger rootLogger = org.apache.log4j.Logger.getRootLogger();
-		Enumeration appenders = rootLogger.getAllAppenders();
-		if ( !appenders.hasMoreElements() ) {
-			LOGGER.log(Level.INFO, "Log4j config file is missing");
-			return false;
-		} else {
-			LOGGER.log(Level.INFO, "Log4j appender found " + ( ( Appender ) appenders.nextElement() ).getName());
-			return true;
+		Enumeration<Appender> appenders = rootLogger.getAllAppenders();
+
+		boolean configured = false;
+		while (appenders.hasMoreElements()) {
+			Appender appender = appenders.nextElement();
+			LOGGER.log(Level.INFO, "Log4j appender found " + appender.getName());
+			if ( !configured ) {
+				configured = true;
+			}
 		}
+		
+		if ( !configured ) {
+			LOGGER.log(Level.INFO, "Log4j config file is missing");
+		}
+		return configured;
 	}
+	
 }
